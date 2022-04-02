a8c69491ee0bab4631c495402ac8d310d742b995
farias_77 <gabrielfalves2003@gmail.com>
2022-04-02 12:01:02 -0300

    nome do prato e valor no modal de fechar pedido / soma dos valores no total

--- scripts.js
+++ scripts.js
@@ -18,11 +18,11 @@ function selecionaOpcao(elementoClicado, classePrato){
     adicionaCheck.classList.remove("escondido");
 
     if(classePrato === ".prato-principal"){
-        PratoSelecionado = true;
+        PratoSelecionado = elementoClicado;
     }else if(classePrato === ".bebida"){
-        BebidaSelecionada = true;
+        BebidaSelecionada = elementoClicado;
     }else if(classePrato === ".sobremesa"){
-        SobremesaSelecionada = true;
+        SobremesaSelecionada = elementoClicado;
     }
 
     if(PratoSelecionado && BebidaSelecionada && SobremesaSelecionada){
@@ -47,10 +47,23 @@ function mudarBotao(){
     mostrarBotao.classList.remove("escondido");
 }
 
-function fecharPedido(){
-
+function fecharPedido(){   
+    
     let modal = document.querySelector(".modalFecharPedido");
     let fundo = document.querySelector(".fundo-transparente")
+    let prato = document.querySelector(".pratoFinal");
+    let bebida = document.querySelector(".bebidaFinal");
+    let sobremesa = document.querySelector(".sobremesaFinal");
+    let total = document.querySelector(".valorTotal");
+    
+    prato.querySelector("p:first-child").innerHTML = `${PratoSelecionado.querySelector("h1").innerHTML}`
+    prato.querySelector("p:last-child").innerHTML = `${retornaSemR$(PratoSelecionado)}`
+    bebida.querySelector("p:first-child").innerHTML = `${BebidaSelecionada.querySelector("h1").innerHTML}`
+    bebida.querySelector("p:last-child").innerHTML = `${retornaSemR$(BebidaSelecionada)}`
+    sobremesa.querySelector("p:first-child").innerHTML = `${SobremesaSelecionada.querySelector("h1").innerHTML}`
+    sobremesa.querySelector("p:last-child").innerHTML = `${retornaSemR$(SobremesaSelecionada)}`
+
+    total.querySelector("p:last-child").innerHTML = `${somaDosValores(retornaSemR$(PratoSelecionado), retornaSemR$(BebidaSelecionada), retornaSemR$(SobremesaSelecionada))}`;
 
     modal.classList.remove("escondido");
     fundo.classList.remove("escondido")
@@ -63,4 +76,36 @@ function cancelaPedido(){
 
     modal.classList.add("escondido");
     fundo.classList.add("escondido")
+}
+
+function somaDosValores(valorPrato, valorBebida, valorSobremesa){
+
+    valorPrato = retornaComPonto(valorPrato);
+    valorBebida = retornaComPonto(valorBebida);
+    valorSobremesa = retornaComPonto(valorSobremesa);
+
+    let valorFinal = (Number(valorPrato) + Number(valorBebida) + Number(valorSobremesa));
+    valorFinal = valorFinal.toFixed(2);
+    valorFinal = valorFinal.toString();
+    valorFinal = retornaComVirgula(valorFinal);
+    valorFinal = `R$ ${valorFinal}`;
+
+    return valorFinal;
+}
+
+function retornaSemR$(elementoSelecionado){
+    let stringCompleta = elementoSelecionado.querySelector("h3").innerHTML;
+    let stringCortada = stringCompleta.slice(3);
+
+    return stringCortada;
+}
+
+function retornaComPonto(elementoComVirgula){
+    elementoComVirgula = elementoComVirgula.replace("," , ".");
+    return elementoComVirgula;
+}
+
+function retornaComVirgula(elementoComPonto){
+    elementoComPonto = elementoComPonto.replace("." , ",");
+    return elementoComPonto;
 }
